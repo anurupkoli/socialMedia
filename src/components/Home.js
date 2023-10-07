ca4e9f228cbc29e2604755953daee15901c33890
Anurup <anurupkoli01@gmail.com>
2023-10-07 18:58:07 +0530

    added navbar component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,10 @@
 import React from 'react'
-import PersonIcon from '@mui/icons-material/Person';
+import Topbar from './topbar/Topbar'
+
 export default function Home() {
   return (
     <>
-        <PersonIcon/>
+        <Topbar/>
     </>
   )
 }
---
+++ src/components/topbar/Topbar.jsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import './topbar.css'
+import SearchIcon from '@mui/icons-material/Search';
+import PersonIcon from '@mui/icons-material/Person';
+import MessageIcon from '@mui/icons-material/Message';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+// import { NavLink } from 'react-router-dom';
+
+export default function Topbar(){
+  return (
+    <div className='navBar' >
+        <div className="leftNavbar">
+            <span>SocialMedia</span>
+        </div>
+        <div className="navBarLinks">
+            <span  className='navBarLinks'>TimeLine</span>
+            <span  className='navBarLinks'>Home</span>
+
+        </div>
+        <div className="middleNavbar">
+            <input placeholder='Search for Friend, Blog, Post, Video' type="text" />
+            <SearchIcon/>
+        </div>
+        <div className="rightNavbar">
+            <div className="rightNavbarItem">
+                <MessageIcon/>
+                <span>1</span>
+            </div>
+            <div className="rightNavbarItem">
+                <PersonIcon/>
+                <span>3</span>
+            </div>
+            <div className="rightNavbarItem">
+                <NotificationsIcon/>
+                <span>2</span>
+            </div>
+        </div>
+        <div className="userProfile">
+            
+        </div>
+    </div>
+  )
+}
