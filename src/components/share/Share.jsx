63a26e003ee6f4e1d385635a5601274d33656c9b
Anurup <anurupkoli01@gmail.com>
2023-10-14 14:22:19 +0530

    Top feed Box Created

--- src/components/share/Share.jsx
+++ src/components/share/Share.jsx
@@ -1,23 +1,45 @@
-import React from 'react'
-import './share.css'
+import React from "react";
+import "./share.css";
+import PermMediaIcon from "@mui/icons-material/PermMedia";
+import LabelIcon from "@mui/icons-material/Label";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import EmojiEmotionsIcon from "@mui/icons-material/EmojiEmotions";
 
 export default function Share() {
   return (
     <>
-        <div className="share">
-            <div className="shareContainer1">
-            <div className="shareTop">
-                <div className="shareImg">
-
-                </div>
-                <input type="text" className='shareText' placeholder="What's on your mind Anurup?" />
+      <div className="share">
+        <div className="shareContainer1">
+          <div className="shareTop">
+            <div className="shareImg"></div>
+            <input
+              type="text"
+              className="shareText"
+              placeholder="What's on your mind Anurup?"
+            />
+          </div>
+          <hr />
+          <div className="shareBottom">
+            <div className="shareTypes">
+              <PermMediaIcon htmlColor="green" />
+              <span>Photo or Video</span>
             </div>
-            <hr />
-            <div className="shareBottom">
-
+            <div className="shareTypes">
+              <LabelIcon htmlColor="blue" />
+              <span>Tag</span>
             </div>
+            <div className="shareTypes">
+              <LocationOnIcon htmlColor="tomato" />
+              <span>Location</span>
             </div>
+            <div className="shareTypes">
+              <EmojiEmotionsIcon htmlColor="goldenrod" />
+              <span>Feelings</span>
+            </div>
+            <button className="shareBtn">Share</button>
+          </div>
         </div>
+      </div>
     </>
-  )
+  );
 }
